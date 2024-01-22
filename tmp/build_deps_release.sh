6745e0f8cdc09735d87bcc510cd0ed702c3f5cc2
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-01-21 19:07:05 -0600

    checkout deps
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- tmp/build_deps_release.sh
+++ tmp/build_deps_release.sh
@@ -5,6 +5,9 @@ BASE_DIR=$(realpath $(dirname ${BASH_SOURCE[0]})/..)
 
 # sudo ${BASE_DIR}/tmp/mount_overlay.sh
 # OVERLAY_PATH=/mnt/efs_overlay/boki \
+cd ${BASE_DIR}
+git submodule update --init --recursive
+cd -
 
 DEPS_INSTALL_PATH=$BASE_DIR/../boki_deps/release \
   ${BASE_DIR}/build_deps.sh
